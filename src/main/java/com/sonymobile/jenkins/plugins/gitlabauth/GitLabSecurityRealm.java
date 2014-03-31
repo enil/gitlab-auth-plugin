29fe07eb2805c3ee8669dd4590f610946fc26a4f
Andreas Alanko <andreas@aalanko.com>
2014-03-31 10:52:29 +0200

    Add code documentation

--- src/main/java/com/sonymobile/jenkins/plugins/gitlabauth/GitLabSecurityRealm.java
+++ src/main/java/com/sonymobile/jenkins/plugins/gitlabauth/GitLabSecurityRealm.java
@@ -18,6 +18,12 @@ import hudson.security.AbstractPasswordBasedSecurityRealm;
 import hudson.security.GroupDetails;
 import hudson.security.SecurityRealm;
 
+/**
+ * A security realm to support the use of login in with GitLab credentials to a Jenkins server.
+ * 
+ * @author Andreas Alanko
+ */
+
 public class GitLabSecurityRealm extends AbstractPasswordBasedSecurityRealm {
 
     @DataBoundConstructor
@@ -25,53 +31,95 @@ public class GitLabSecurityRealm extends AbstractPasswordBasedSecurityRealm {
         super();
     }
 
+    /**
+     * Specifies if the configured Security Realm allows signup.
+     * 
+     * @return true if signup is allowed
+     */
     @Override
     public boolean allowsSignup() {
         return false;
     }
 
+    /**
+     * Tries to authenticate a user with the given username and password.
+     * 
+     * @param username the username of the user
+     * @param password the password of the user
+     * @return a UserDetails object with user information.
+     * @throws AuthenticationException if the authentication fails
+     */
     @Override
-    protected UserDetails authenticate(String username, String password)
-            throws AuthenticationException {
+    protected UserDetails authenticate(String username, String password) throws AuthenticationException {
         UserDetails userDetails = null;
 
         if (isValidUser(username, password)) {
             userDetails = new User(username, password, true, true, true, true, new GrantedAuthority[] { SecurityRealm.AUTHENTICATED_AUTHORITY });
         } else {
-            throw new BadCredentialsException(
-                    "Not a valid username or password");
+            throw new BadCredentialsException("Not a valid username or password");
         }
 
         return userDetails;
     }
 
-    /*
-     * Checks user credentials with GitLab API server
+    /**
+     * Checks if a user with matching username and password exists.
+     * 
+     * @param username the username of the user
+     * @param password  the password of the user
+     * @return true if there exist a user with username and a matching password
      */
     private boolean isValidUser(String username, String password) {
         return username.equals("nisse") && password.equals("p2");
     }
-
+    
+    /**
+     * Gets user information about the user with the given username.
+     * 
+     * @param username the user of the user
+     * @return a UserDetails object with information about the user
+     * @throws UsernameNotFoundException if user with username does not exist
+     * @throws DataAccessException will never be thrown
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException, DataAccessException {
-        // TODO Auto-generated method stub
+        //TODO
         return null;
     }
 
+    /**
+     * This feature is not supported.
+     * Will throw UsernameNotFoundException at all times.
+     * 
+     * @params username the username of the user
+     * @throws UsernameNotFoundException will be thrown at all times.
+     * @throws DataAccessException will never be thrown.
+     */
     @Override
-    public GroupDetails loadGroupByGroupname(String groupname)
-            throws UsernameNotFoundException, DataAccessException {
+    public GroupDetails loadGroupByGroupname(String groupname) throws UsernameNotFoundException, DataAccessException {
         throw new UsernameNotFoundException("Feature not supported");
     }
 
     @Extension
     public static final class DescriptorImpl extends Descriptor<SecurityRealm> {
 
+        /**
+         * Returns a new GitLabSecurityRealm object
+         * 
+         * @param req the http request
+         * @param formData form data f
+         * @return a GitLabSecurityRealm object
+         */
         @Override
         public SecurityRealm newInstance(StaplerRequest req, JSONObject formData) throws FormException {
             return new GitLabSecurityRealm();
         }
 
+        /**
+         * Gives the name to be displayed by the Jenkins view in the security configuration page.
+         * 
+         * @return the display name
+         */
         public String getDisplayName() {
             return "GitLab Authentication";
         }
--- src/main/java/com/sonymobile/jenkins/plugins/gitlabauth/GitLabUserInformation.java
+++ src/main/java/com/sonymobile/jenkins/plugins/gitlabauth/GitLabUserInformation.java
@@ -5,40 +5,85 @@ import hudson.model.User;
 import hudson.model.UserProperty;
 import hudson.model.UserPropertyDescriptor;
 
+/**
+ * UserProperty used to display GitLab user information on the user summary page.
+ * GitLabUserInformation/summary.jelly is used to display the information.
+ * 
+ * @author Andreas Alanko
+ */
+
 public class GitLabUserInformation extends UserProperty {
+    /** the logged in user object with GitLab information */
     private User mUser;
 
+    /**
+     * Creates a new object for the given user. 
+     * The constructor gets the GitLab information for the given user.
+     * 
+     * @param user the logged in user
+     */
     public GitLabUserInformation(User user) {
-        /*
-         * Should be a GitLabUser instead
-         */
+        //TODO: Get GitLabUser object with information
         this.mUser = user;
     }
 
+    /**
+     * Gets the GitLab username of the logged in user.
+     * 
+     * @return the username or "Anonymous" if the user does not exist.
+     */
     public String getUsername() {
-        return (mUser != null) ? mUser.getDisplayName() : "Anon";
+        return (mUser != null) ? mUser.getDisplayName() : "Anonymous";
     }
 
+    /**
+     * Gets the GitLab full name of the logged in user.
+     * 
+     * @return the full name or the username if a full name is not set.
+     */
     public String getFullname() {
         return (mUser != null) ? mUser.getFullName() : this.getUsername();
     }
 
+    /**
+     * Gets the GitLab email address of the logged in user.
+     * 
+     * @return the email adress
+     */
     public String getEmail() {
+        //TODO
         return "Not available";
     }
 
     @Extension
     public static final class DescriptorImpl extends UserPropertyDescriptor {
 
+        /**
+         * Gets the display name for the configuration page.
+         * 
+         * @return the display name
+         */
         public String getDisplayName() {
             return "GitLab user information";
         }
 
+        /**
+         * Creates a new instance of the GitLabUserInformation object containing information about the logged in user.
+         * 
+         * @return the UserPropery object
+         */
         @Override
         public UserProperty newInstance(User user) {
             return new GitLabUserInformation(user);
         }
 
+        /**
+         * Check to see if the UserProperty is enabled.
+         * 
+         * This is set to not be enabled because we don't want to enable any configuration and only display user information from GitLab.
+         * 
+         * @return true if the plugin is enabled.
+         */
         public boolean isEnabled() {
             return false;
         }
