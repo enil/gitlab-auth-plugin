c146445b94230937c85499747d2d55551b9f964e
Andreas Alanko <andreas.alanko@sonymobile.com>
2014-04-16 20:39:11 +0200

    Fix for-looping in groovy view

--- src/main/resources/com/sonymobile/jenkins/plugins/gitlabauth/GitLabAuthorization/config.groovy
+++ src/main/resources/com/sonymobile/jenkins/plugins/gitlabauth/GitLabAuthorization/config.groovy
@@ -5,8 +5,9 @@ def j = namespace("jelly:core")
 
 f.block() {
     link(rel: "stylesheet", href: "/plugin/gitlab-auth/table.css", type: "text/css")
-    j.set(var: "permissionGroups", value: descriptor.allPermissionGroups)
-    
+
+    def permissionGroups = descriptor.allPermissionGroups
+
     table("class": "center-align global-matrix-authorization-strategy-table", name: "permissionTable") {
         tr {
             td("class": "pane-header", rowspan: "2") {
@@ -14,43 +15,35 @@ f.block() {
                 br()
                 text("Role")
             }
-            
-            j.forEach(var: "pg", items: permissionGroups.keySet()) {
-                td("class": "pane-header", colspan: permissionGroups.get(pg).size()) {
-                    text(pg.title)
-                }
+
+            for (pg in permissionGroups.keySet()) {
+                td("class": "pane-header", colspan: permissionGroups.get(pg).size()) { text(pg.title) }
             }
         }
-        
+
         tr {
-            j.forEach(var: "pg", items: permissionGroups.keySet()) {
-                j.forEach(var: "p", items: permissionGroups.get(pg)) {
-                    td {
-                        text(p.name)
-                    }
+            for (pg in permissionGroups.keySet()) {
+                for (p in permissionGroups.get(pg)) {
+                    td { text(p.name) }
                 }
             }
         }
-        
-        j.set(var: "roles", value: descriptor.allRoles)
-        
-        j.forEach(var: "role", items: roles) {
+
+        for (role in descriptor.allRoles) {
             tr(name: role) {
-                td {
-                    text(role)
-                }
-                
-                j.forEach(var: "pg", items: permissionGroups.keySet()) {
-                    j.forEach(var: "p", items: permissionGroups.get(pg)) {
+                td { text(role) }
+
+                for (pg in permissionGroups.keySet()) {
+                    for (p in permissionGroups.get(pg)) {
                         td {
-                            f.checkbox(name: p.id, checked: instance.isPermissionSet(role, p))
+                            f.checkbox(name: "["+p.id+"]", checked: (instance != null) ? instance.isPermissionSet(role, p) : false)
                         }
                     }
                 }
             }
         }
     }
-    
+
     f.entry(title: "Admin usernames", description: "Usernames of GitLab users who will be granted admin rights on the Jenkins server. Usernames should be seperated by commas.") {
         f.checkbox(field: "useGitLabAdmins", title: "Check if all GitLab admins also should be Jenkins admins.")
         f.textbox(field: "adminUsernames")
