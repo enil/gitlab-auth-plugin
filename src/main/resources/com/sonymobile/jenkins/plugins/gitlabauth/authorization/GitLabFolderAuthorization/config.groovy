ab461357c81bebfec429dee76554ba2520048f18
Andreas Alanko <andreas.alanko@sonymobile.com>
2014-05-14 13:06:19 +0200

    Fix FolderACL matrix should only be visable when created by the system

--- src/main/resources/com/sonymobile/jenkins/plugins/gitlabauth/authorization/GitLabFolderAuthorization/config.groovy
+++ src/main/resources/com/sonymobile/jenkins/plugins/gitlabauth/authorization/GitLabFolderAuthorization/config.groovy
@@ -6,79 +6,80 @@ def st = namespace("jelly:stapler")
 
 def itemPermissionGroup = descriptor.itemPermissionGroup
 
-f.section(title: "GitLab Group") {
-    f.block {
-        div {
-            b { text "Group ID: " }
-            text instance?.groupId
-        }
-        div {
-            b { text "Group name: " }
-            text instance?.groupName
-        }
-        div {
-            b { text "Group path: " }
-            text instance?.groupPath
+if (instance?.groupId) {
+    f.section(title: "GitLab Group") {
+        f.block {
+            div {
+                b { text "Group ID: " }
+                text instance?.groupId
+            }
+            div {
+                b { text "Group name: " }
+                text instance?.groupName
+            }
+            div {
+                b { text "Group path: " }
+                text instance?.groupPath
+            }
         }
     }
-}
-
-f.section(title: "GitLab Folder Authorization") {
-    f.block {
-        link(rel: "stylesheet", href: rootURL+"/plugin/gitlab-auth/table.css", type: "text/css")
-        script(type: "text/javascript", src: rootURL+"/plugin/gitlab-auth/authorization.matrix.js")
-
-        table("class": "center-align global-matrix-authorization-strategy-table", name: "permissionTable", id: "permissionTable") {
-            tr {
-                td("class": "pane-header", rowspan: "2") {
-                    text("Permission")
-                    br()
-                    text("Role")
-                }
-                
-                td("class": "pane-header", colspan: itemPermissionGroup.permissions.size()) {
-                    text(itemPermissionGroup.title)
-                }
-            }
-            
-            tr {
-                itemPermissionGroup.each { permission ->
-                    td {
-                        script(type: "text/javascript") {
-                            text('addItem("' + permission.id + '")')
-                        }
-                        
-                        if (permission.enabled) {
-                            text(permission.name)
-                        } else {
-                            i { text(permission.name) }
-                        }
-                    }
-                }
-            }
-
-            def identities = (instance != null) ? instance.folderPermissionIdentities : descriptor.staticPermissionIdentities
-            
-            identities.each { identity ->
-                tr(name: identity) {
-                    td {
-                        text(identity.displayName)
+    
+    f.section(title: "GitLab Folder Authorization") {
+        f.block {
+            link(rel: "stylesheet", href: rootURL+"/plugin/gitlab-auth/table.css", type: "text/css")
+            script(type: "text/javascript", src: rootURL+"/plugin/gitlab-auth/authorization.matrix.js")
+    
+            table("class": "center-align global-matrix-authorization-strategy-table", name: "permissionTable", id: "permissionTable") {
+                tr {
+                    td("class": "pane-header", rowspan: "2") {
+                        text("Permission")
+                        br()
+                        text("Role")
                     }
                     
-                    itemPermissionGroup.permissions.each { permission ->
+                    td("class": "pane-header", colspan: itemPermissionGroup.permissions.size()) {
+                        text(itemPermissionGroup.title)
+                    }
+                }
+                
+                tr {
+                    itemPermissionGroup.each { permission ->
                         td {
-                            f.checkbox(name: "[${permission.id}]", checked: instance?.isPermissionSet(identity, permission))
+                            script(type: "text/javascript") {
+                                text('addItem("' + permission.id + '")')
+                            }
+                            
+                            if (permission.enabled) {
+                                text(permission.name)
+                            } else {
+                                i { text(permission.name) }
+                            }
+                        }
+                    }
+                }
+    
+                def identities = (instance != null) ? instance.folderPermissionIdentities : descriptor.staticPermissionIdentities
+                
+                identities.each { identity ->
+                    tr(name: identity) {
+                        td {
+                            text(identity.displayName)
+                        }
+                        
+                        itemPermissionGroup.permissions.each { permission ->
+                            td {
+                                f.checkbox(name: "[${permission.id}]", checked: instance?.isPermissionSet(identity, permission))
+                            }
                         }
                     }
                 }
             }
-        }
-        
-        f.entry(title: "Add group/user:") {
-            input(type: "text", id: "addUserGroupText", style: "width: 200px")
-            input(type: "button", id: "addGroupRowButton", onclick: "addGroupRow()", value: "Add group")
-            input(type: "button", id: "addUserRowButton", onclick: "addUserRow()", value: "Add user")
+            
+            f.entry(title: "Add group/user:") {
+                input(type: "text", id: "addUserGroupText", style: "width: 200px")
+                input(type: "button", id: "addGroupRowButton", onclick: "addGroupRow()", value: "Add group")
+                input(type: "button", id: "addUserRowButton", onclick: "addUserRow()", value: "Add user")
+            }
         }
     }
 }
-
