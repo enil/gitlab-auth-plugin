cf818bd7eac2baf6afcabb7d6fe0b72473e0d940
Emil Nilsson <Emil.Nilsson@sonymobile.com>
2014-05-22 14:13:32 +0200

    Create class GroupFolderManager
    Create the class GroupFolderManager to replace GitLabFolderCreator and
    the test class GroupFolderManagerTest.

---
+++ src/main/java/com/sonymobile/jenkins/plugins/gitlabauth/folder/GroupFolderManager.java
@@ -0,0 +1,207 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Andreas Alanko, Emil Nilsson, Sony Mobile Communications AB.
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.sonymobile.jenkins.plugins.gitlabauth.folder;
+
+import com.cloudbees.hudson.plugins.folder.Folder;
+import com.sonymobile.gitlab.model.GitLabGroupInfo;
+import com.sonymobile.jenkins.plugins.gitlabauth.authorization.GitLabFolderAuthorization;
+import com.sonymobile.jenkins.plugins.gitlabauth.exceptions.ItemNameCollisionException;
+import hudson.model.TopLevelItem;
+import hudson.model.TopLevelItemDescriptor;
+import jenkins.model.Jenkins;
+import jenkins.model.ModifiableTopLevelItemGroup;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import static org.apache.commons.lang3.StringUtils.join;
+
+/**
+ * Singleton class for managing GitLab group folders.
+ *
+ * @author Emil Nilsson
+ */
+public class GroupFolderManager {
+    /** The singleton instance. */
+    private static final GroupFolderManager INSTANCE = new GroupFolderManager();
+
+    /** The item group for the folders. */
+    private final ModifiableTopLevelItemGroup itemGroup;
+
+    /** The descriptor of the folder item. */
+    private final TopLevelItemDescriptor folderDescriptor;
+
+    /**
+     * Creates a GitLab group folder manager.
+     */
+    private GroupFolderManager() {
+        this(getJenkinsInstance(), getFolderDescriptor());
+    }
+
+    /**
+     * Creates a GitLab group folder manager with a custom item group and folder descriptor.
+     *
+     * @param itemGroup        the item group
+     * @param folderDescriptor the folder descriptor
+     */
+    private GroupFolderManager(ModifiableTopLevelItemGroup itemGroup, TopLevelItemDescriptor folderDescriptor) {
+        this.itemGroup = itemGroup;
+        this.folderDescriptor = folderDescriptor;
+    }
+
+    /**
+     * Returns the singleton instance of the class.
+     *
+     * @return the singleton instance
+     */
+    public GroupFolderManager getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     * Returns all GitLab folders.
+     *
+     * @return a map of group IDs and folders
+     */
+    public synchronized Map<Integer, Folder> getFolders() {
+        Map<Integer, Folder> existingFolders = new TreeMap<Integer, Folder>();
+
+        for (final TopLevelItem item : itemGroup.getItems()) {
+            if (item instanceof Folder) {
+                Folder folder = (Folder)item;
+                GitLabFolderAuthorization property = folder.getProperties().get(GitLabFolderAuthorization.class);
+
+                // make sure the GitLab authorization property is set folder
+                if (property != null) {
+                    existingFolders.put(property.getGroupId(), folder);
+                }
+            }
+        }
+
+        return existingFolders;
+    }
+
+    /**
+     * Creates folders for a collection of GitLab groups.
+     *
+     * Checks if folders already exists before attempting to create a new folder.
+     *
+     * @param groups the groups
+     * @throws ItemNameCollisionException if an item names for new folders already were in use
+     * @throws IOException                if saving to persistent storage failed
+     */
+    public synchronized void createFolders(Iterable<GitLabGroupInfo> groups)
+            throws ItemNameCollisionException, IOException {
+        // get existing group folders
+        Map<Integer, Folder> existingFolders = getFolders();
+
+        // groups which item names collide with existing items
+        List<String> collidedGroupPaths = new LinkedList<String>();
+
+        for (GitLabGroupInfo group : groups) {
+            try {
+                // only create folders that don't already exist
+                if (!existingFolders.containsKey(group.getId())) {
+                    createFolder(group);
+                }
+            } catch (ItemNameCollisionException e) {
+                collidedGroupPaths.add(group.getPath());
+            }
+        }
+
+        if (!collidedGroupPaths.isEmpty()) {
+            throw new ItemNameCollisionException("Cannot create folder(s) because items with the names "
+                    + join(collidedGroupPaths, ", ") + " already exist(s)");
+        }
+    }
+
+    /**
+     * Checks if a folder for a GitLab group exists.
+     *
+     * @param group the group
+     * @return true if the folder exists
+     */
+    public synchronized boolean folderExists(GitLabGroupInfo group) {
+        int groupId = group.getId();
+
+        for (final TopLevelItem item : itemGroup.getItems()) {
+            if (item instanceof Folder) {
+                Folder folder = (Folder)item;
+                GitLabFolderAuthorization property = folder.getProperties().get(GitLabFolderAuthorization.class);
+
+                if (property != null && property.getGroupId() == groupId) {
+                    // folder matches the group
+                    return true;
+                }
+            }
+        }
+
+        // not found
+        return false;
+    }
+
+    /**
+     * Creates a folder for a GitLab group.
+     *
+     * Does not check if a folder already exists for the GitLab group.
+     *
+     * @param group the group
+     * @throws ItemNameCollisionException if an item name for a new folder already was in use
+     * @throws IOException                if saving to persistent storage failed
+     */
+    private void createFolder(GitLabGroupInfo group)
+            throws ItemNameCollisionException, IOException {
+        try {
+            Folder folder = (Folder)itemGroup.createProject(folderDescriptor, group.getPath(), true);
+            folder.addProperty(new GitLabFolderAuthorization(group.getId()));
+        } catch (IllegalArgumentException e) {
+            throw new ItemNameCollisionException("Cannot create folder because an item with the name "
+                    + group.getPath() + " already exists");
+        }
+    }
+
+    /**
+     * Gets the Jenkins instance
+     *
+     * @return the Jenkins instance
+     */
+    private static Jenkins getJenkinsInstance() {
+        return Jenkins.getInstance();
+    }
+
+    /**
+     * Gets the descriptor of {@link com.cloudbees.hudson.plugins.folder.Folder}
+     *
+     * @return the descriptor
+     */
+    private static TopLevelItemDescriptor getFolderDescriptor() {
+        return getJenkinsInstance().getDescriptorByType(Folder.DescriptorImpl.class);
+    }
+}
---
+++ src/test/java/com/sonymobile/jenkins/plugins/gitlabauth/folder/GroupFolderManagerTest.java
@@ -0,0 +1,249 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Andreas Alanko, Emil Nilsson, Sony Mobile Communications AB.
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.sonymobile.jenkins.plugins.gitlabauth.folder;
+
+import com.cloudbees.hudson.plugins.folder.Folder;
+import com.sonymobile.gitlab.model.GitLabGroupInfo;
+import com.sonymobile.jenkins.plugins.gitlabauth.authorization.GitLabFolderAuthorization;
+import com.sonymobile.jenkins.plugins.gitlabauth.exceptions.ItemNameCollisionException;
+import hudson.model.TopLevelItem;
+import hudson.model.TopLevelItemDescriptor;
+import jenkins.model.ModifiableTopLevelItemGroup;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import static com.sonymobile.jenkins.plugins.gitlabauth.helpers.MockDataCreators.expectNewFolderAuthorization;
+import static com.sonymobile.jenkins.plugins.gitlabauth.helpers.MockDataCreators.mockFolderAuthorization;
+import static com.sonymobile.jenkins.plugins.gitlabauth.helpers.MockDataCreators.mockGroupInfo;
+import static com.sonymobile.jenkins.plugins.gitlabauth.helpers.MockFolderBuilder.folder;
+import static com.sonymobile.jenkins.plugins.gitlabauth.helpers.MockFolderBuilder.mockFolder;
+import static com.sonymobile.jenkins.plugins.gitlabauth.helpers.MockFreeStyleProjectBuilder.freeStyleProject;
+import static java.util.Arrays.asList;
+import static org.apache.commons.lang.StringUtils.EMPTY;
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.hasKey;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Tests for {@link GroupFolderManager}
+ *
+ * @author Emil Nilsson
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ GitLabFolderAuthorization.class, GroupFolderManager.class })
+public class GroupFolderManagerTest {
+    /** A rule for catching expected exceptions. */
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    /** The descriptor for a folder item. */
+    private static final TopLevelItemDescriptor folderDescriptor = new Folder.DescriptorImpl();
+
+    /** The target item group. */
+    private ModifiableTopLevelItemGroup itemGroup;
+
+    /** Items in the target item group. */
+    private List<TopLevelItem> items;
+
+    /** The GitLab groups. */
+    private List<GitLabGroupInfo> groups;
+
+    /** The folder manager instance. */
+    private GroupFolderManager folderManager;
+
+    @Before
+    public void setUp() throws Exception {
+        items = new LinkedList<TopLevelItem>();
+        groups = new LinkedList<GitLabGroupInfo>();
+
+        // mock itemGroup#getGetItems() returning the items list
+        itemGroup = createMock(ModifiableTopLevelItemGroup.class);
+        expect(itemGroup.getItems()).andReturn(items).anyTimes();
+
+        // mock creating new GitLabFolderAuthorization folder properties
+        expectNewFolderAuthorization();
+
+        // create a folder manager with the mock item group and folder descriptor
+        folderManager = Whitebox.invokeConstructor(
+                GroupFolderManager.class,
+                new Class[]{ ModifiableTopLevelItemGroup.class, TopLevelItemDescriptor.class },
+                new Object[]{ itemGroup, folderDescriptor });
+    }
+
+    /**
+     * Tests getting the existing GitLab group folders.
+     */
+    @Test
+    public void getFolders() {
+        // two GitLab folder and two misc items
+        addItems(
+                gitLabFolder("group1", 1),
+                gitLabFolder("group2", 2),
+                folder("folder"),
+                freeStyleProject("item"));
+        replay(itemGroup);
+
+        Map<Integer, Folder> existingFolders = folderManager.getFolders();
+
+        verify(itemGroup);
+
+        assertThat(existingFolders.size(), is(2));
+        assertThat("GitLab folder not matched", existingFolders, hasKey(1));
+        assertThat("GitLab folder not matched", existingFolders, hasKey(2));
+
+        assertThat(existingFolders.get(1).getName(), is("group1"));
+        assertThat(existingFolders.get(2).getName(), is("group2"));
+    }
+
+    /**
+     * Tests creating new GitLab group folders.
+     */
+    @Test
+    public void createFolders() throws Exception {
+        // attempt to create group1 and group2
+        addGroups(
+                mockGroupInfo(1, "Group 1", "group1"),
+                mockGroupInfo(2, "Group 2", "group2")
+        );
+        // group1 is already created
+        addItems(
+                gitLabFolder("group1", 1),
+                folder("folder"),
+                freeStyleProject("item"));
+
+        // return group2 when creating a new folder
+        Folder group2 = folder("group2");
+        expect(itemGroup.createProject(folderDescriptor, "group2", true))
+                .andReturn(group2);
+        replay(itemGroup);
+
+        folderManager.createFolders(groups);
+
+        verify(itemGroup);
+
+        // get property for group2
+        GitLabFolderAuthorization folderProperty = group2.getProperties().get(GitLabFolderAuthorization.class);
+
+        assertThat("name not set correctly", group2.getName(), is("group2"));
+        assertThat("folder property not set", folderProperty, is(notNullValue()));
+        assertThat("group ID not set correctly", folderProperty.getGroupId(), is(2));
+    }
+
+    /**
+     * Tests creating new GitLab group folders when the item name collides with another item.
+     */
+    @Test
+    public void createFoldersWithNameCollisions() throws Exception {
+        // attempt to create group1, group2, group3 and group4
+        addGroups(
+                mockGroupInfo(1, "Group 1", "group1"),
+                mockGroupInfo(2, "Group 2", "group2"),
+                mockGroupInfo(3, "Group 3", "group3"),
+                mockGroupInfo(3, "Group 4", "group4")
+        );
+        // group1 is already created, group3 and group4 collides with existing items
+        addItems(
+                gitLabFolder("group1", 1),
+                freeStyleProject("group3"),
+                folder("group4"));
+
+        expect(itemGroup.createProject(folderDescriptor, "group2", true))
+                .andReturn(folder("group2"));
+        expect(itemGroup.createProject(folderDescriptor, "group3", true))
+                .andThrow(new IllegalArgumentException(EMPTY));
+        expect(itemGroup.createProject(folderDescriptor, "group4", true))
+                .andThrow(new IllegalArgumentException(EMPTY));
+        replay(itemGroup);
+
+        // should throw an exception because the item names collides
+        thrown.expect(ItemNameCollisionException.class);
+        thrown.expectMessage(containsString("group3, group4"));
+
+        folderManager.createFolders(groups);
+
+        verify(itemGroup);
+    }
+
+    /**
+     * Tests checking if GitLab group folders already exist.
+     */
+    @Test
+    public void folderExists() {
+        // only group1 is a GitLab group
+        addItems(
+                gitLabFolder("group1", 1),
+                folder("group2"),
+                freeStyleProject("group3"));
+        replay(itemGroup);
+
+        assertThat(folderManager.folderExists(mockGroupInfo(1, "Group 1", "group1")), is(true));
+        assertThat(folderManager.folderExists(mockGroupInfo(2, "Group 2", "group2")), is(false));
+        assertThat(folderManager.folderExists(mockGroupInfo(3, "Group 3", "group3")), is(false));
+        assertThat(folderManager.folderExists(mockGroupInfo(4, "Group 4", "group4")), is(false));
+
+        verify(itemGroup);
+    }
+
+    /**
+     * Creates a mock of a GitLab group folder
+     *
+     * @param name    the name of the item
+     * @param groupId the group ID of the group
+     * @return a GitLab group folder
+     */
+    private Folder gitLabFolder(String name, int groupId) {
+        return mockFolder().name(name).addProperty(mockFolderAuthorization(groupId)).build();
+    }
+
+    /**
+     * Adds items to the target item group.
+     *
+     * @param items the items
+     */
+    private void addItems(TopLevelItem... items) {
+        this.items.addAll(asList(items));
+    }
+
+    private void addGroups(GitLabGroupInfo... groups) {
+        this.groups.addAll(asList(groups));
+    }
+}
